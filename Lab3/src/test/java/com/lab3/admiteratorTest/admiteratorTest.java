6a1d95157eeeb92ecaa8f8ea899826a00f25f3f5
tuf33757 <tuf33757@temple.edu>
2015-03-25 10:39:17 -0400

    updated

---
+++ Lab3/src/test/java/com/lab3/admiteratorTest/admiteratorTest.java
@@ -0,0 +1,16 @@
+package com.lab3.admiteratorTest;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+@RunWith(JUnitParamsRunner.class)
+
+public class admiteratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
